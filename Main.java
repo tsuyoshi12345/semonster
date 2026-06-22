b476d1e68e7ac340603feba0c89b4a4cb8773a9e
tsuyoshi12345 <m1m02530112@oit.ac.jp>
2026-06-22 19:36:24 +0900

    シナリオ2:v2.0に向けた機能追加とコミット

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello SEMonster");
     System.out.println("Hello Hello");
+
+    Player player = new Player();
+
+    player.drawMonsters();
+    player.showDeck();
   }
 }
