aed2449e579a0bff679394ccc23330af411e9cdb
tsuyoshi12345 <m1m02530112@oit.ac.jp>
2026-06-28 16:35:02 +0900

    競合を修正したファイルをcommit

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster GAME");
-    System.out.println("Show a new monster!");
-
+    System.out.println("SEMonster!");
     Player player = new Player();
-
     player.drawMonsters();
     player.showDeck();
+
   }
 }
